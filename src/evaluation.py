e3a160dd4758527c89f198e7e6b94b42ef1f7b69
jakub1090cn <jakubcymerman505@gmail.com>
2024-09-05 17:14:05 +0200

    feat: Add evaluating model

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 def make_predictions(model, dataset):
@@ -14,6 +15,15 @@ def make_predictions(model, dataset):
     y_pred = np.array(y_pred)
 
     return y_true, y_pred
+
+
+def evaluate_model(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average='weighted')
+    recall = recall_score(y_true, y_pred, average='weighted')
+    f1 = f1_score(y_true, y_pred, average='weighted')
+
+    return accuracy, precision, recall, f1
 
 
 
