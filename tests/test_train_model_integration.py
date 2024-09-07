650b529921d82032e0970b592d1d70b110394ed4
jakub1090cn <jakubcymerman505@gmail.com>
2024-09-07 10:12:52 +0200

    test: Add testing number of epochs

--- tests/test_train_model_integration.py
+++ tests/test_train_model_integration.py
@@ -1,12 +1,8 @@
-from unittest.mock import patch, MagicMock
+import pytest
+from src.config import BoldOrNotConfig
 from src.model_training import train_model
 
-@patch('src.model_training.BaldOrNotModel.fit')
-@patch('src.model_training.BaldDataset')
-def test_train_model(MockBaldDataset, MockFit, test_config):
-    MockFit.return_value = MagicMock()
 
-    history = train_model(test_config)
-
-    MockFit.assert_called_once()
-    assert history is not None
+def test_num_of_epochs(test_config: BoldOrNotConfig) -> None:
+    history = train_model(config=test_config)
+    assert len(history.epoch) == test_config.training_params.epochs
