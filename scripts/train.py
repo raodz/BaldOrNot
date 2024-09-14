24fd7b99fcf575aade387eee391d72f629a75419
jakub1090cn <jakubcymerman505@gmail.com>
2024-09-14 11:47:22 +0200

    chore: Add logging instead of prints

--- scripts/train.py
+++ scripts/train.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import shutil
 from jsonargparse import CLI
 import json
@@ -10,23 +11,43 @@ from src.plot import plot_metric_curve
 
 
 def run_experiment(config: BoldOrNotConfig):
+    logging.info("Initializing output directory...")
     output_dir_path = init_output_dir()
+
+    logging.info("Setting up logging...")
     setup_logging(output_dir_path)
+
     config_path = os.path.join(output_dir_path, "config.yaml")
+    logging.info(f"Copying configuration file to output directory: {config_path}")
     shutil.copy("../config.yaml", config_path)
 
-    print("Running experiment with the following configuration:")
-    print(json.dumps(asdict(config), indent=4))
-    print("Start training:")
+    logging.info(f"Running experiment with the following configuration:\n"
+                 f"{json.dumps(asdict(config), indent=4)}")
+
+    logging.info("Starting model training...")
     history = train_model(config=config, output_dir_path=output_dir_path)
+
+    logging.info("Model training completed. History of training:\n"
+                 f"{json.dumps(history, indent=4)}")
+
+    logging.info("Plotting metric curve for loss...")
     plot_metric_curve(
         history=history, metric="loss", output_dir_path=output_dir_path
     )
+    logging.info("Loss curve saved.")
+
+    logging.info("Plotting metric curve for accuracy...")
     plot_metric_curve(
         history=history, metric="accuracy", output_dir_path=output_dir_path
     )
+    logging.info("Accuracy curve saved.")
+
+    logging.info("Experiment completed successfully.")
 
 
 if __name__ == "__main__":
+    logging.info("Parsing configuration from CLI...")
     config = CLI(BoldOrNotConfig)
+
+    logging.info("Running experiment with parsed configuration.")
     run_experiment(config=config)
