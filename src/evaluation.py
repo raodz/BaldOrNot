dd3097571bd689557613359aed69746909c9704d
jakub1090cn <jakubcymerman505@gmail.com>
2024-09-16 22:09:17 +0200

    chore: change way of making predictions

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,18 +6,19 @@ from sklearn.metrics import (
     f1_score,
     confusion_matrix,
 )
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 def make_predictions(model, dataset) -> tuple[np.ndarray, np.ndarray]:
     """
-    Makes predictions on the provided dataset and returns the true and
+    Makes y_pred on the provided dataset and returns the true and
     predicted labels.
 
     Args:
-        model: The trained machine learning model used for making predictions.
-        dataset: The dataset to make predictions on. This should be an iterable
+        model: The trained machine learning model used for making y_pred.
+        dataset: The dataset to make y_pred on. This should be an iterable
             of (images, labels).
 
     Returns:
@@ -25,23 +26,21 @@ def make_predictions(model, dataset) -> tuple[np.ndarray, np.ndarray]:
         - y_true: The true labels.
         - y_pred: The predicted labels.
     """
-    y_true = []
-    y_pred = []
+    images, y_true = zip(*dataset)
+    images = np.concatenate(images, axis=0)
+    y_true = np.concatenate(y_true, axis=0)
 
-    for images, labels in dataset:
-        predictions = model.predict(images)
-        y_pred.extend(np.argmax(predictions, axis=1))
-        y_true.extend(labels)
+    y_pred = model.predict(images)
 
-    y_true = np.array(y_true)
-    y_pred = np.array(y_pred)
+    if isinstance(y_pred, tf.Tensor):
+        y_pred = y_pred.numpy()
 
     return y_true, y_pred
 
 
 def evaluate_model(
     y_true: np.ndarray, y_pred: np.ndarray
-) -> dict[str: float, str: float, str:float, str:float]:
+) -> dict[str: float, str: float, str:float, str:float, str:np.ndarray]:
     """
     Evaluates the model performance by calculating various metrics.
 
@@ -60,11 +59,13 @@ def evaluate_model(
     precision = precision_score(y_true, y_pred, average="weighted")
     recall = recall_score(y_true, y_pred, average="weighted")
     f1 = f1_score(y_true, y_pred, average="weighted")
+    conf_matrix = confusion_matrix(y_true, y_pred)
 
     return {'accuracy': accuracy,
             'precision': precision,
             'recall': recall,
-            'f1_score': f1}
+            'f1_score': f1,
+            'conf_matrix': conf_matrix}
 
 
 def get_misclassifications(
@@ -93,8 +94,7 @@ def get_misclassifications(
 
 
 def plot_confusion_matrix(
-    y_true: np.ndarray,
-    y_pred: np.ndarray,
+    confussion_matrix: np.ndarray,
     class_names: list[str],
     output_path: str,
 ) -> None:
@@ -110,11 +110,10 @@ def plot_confusion_matrix(
     Returns:
         None
     """
-    matrix = confusion_matrix(y_true, y_pred)
 
     plt.figure(figsize=(10, 7))
     sns.heatmap(
-        matrix,
+        confussion_matrix,
         annot=True,
         fmt="d",
         cmap="Blues",
