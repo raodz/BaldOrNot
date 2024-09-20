895363008c6dce0eb561d4d48dd31ee798a73c0b
raodz <raodziem@gmail.com>
2024-09-20 18:09:43 +0200

    chore: delete unnecessary constant

--- src/constants.py
+++ src/constants.py
@@ -4,4 +4,3 @@ N_CHANNELS_RGB = 3
 N_CHANNELS_GRAYSCALE = 1
 DEFAULT_IMG_SIZE = (224, 224)
 LOG_FILE_NAME = "training.log"
-PROJECT_PATH = "os.path.dirname(os.path.dirname(os.path.abspath(__file__)))"
