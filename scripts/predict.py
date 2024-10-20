958e4166fe98933d6638c329f03b5639cbf226e8
jakub1090cn <jakubcymerman505@gmail.com>
2024-10-20 19:33:25 +0200

    chore: used method from BaldDataset instead of dedicated function to preprocess image

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,12 +1,11 @@
 import tensorflow as tf
+import os
 from jsonargparse import CLI
-
+import numpy as np
 from src.config_class import PredictConfig
-from src.predict_hairstyle import (
-    load_image_to_predict,
-    preprocess_image_for_model,
-    make_prediction,
-)
+from src.constants import DEFAULT_IMG_SIZE, N_CHANNELS_RGB
+from src.data import BaldDataset
+from src.predict_hairstyle import make_prediction
 
 
 if __name__ == "__main__":
@@ -16,10 +15,12 @@ if __name__ == "__main__":
     image_dir_path = config.predict_params.image_dir_path
     image_name = config.predict_params.image_name
 
-    image = load_image_to_predict(image_dir_path, image_name)
-    preprocessed_image = preprocess_image_for_model(image)
-
+    image_path = os.path.join(image_dir_path, image_name)
+    preprocessed_image = BaldDataset.preprocess_image(
+        image_path, DEFAULT_IMG_SIZE, N_CHANNELS_RGB
+    )
+    img_batch = np.expand_dims(preprocessed_image, axis=0)
     model = tf.keras.models.load_model(model_path)
 
-    prediction = make_prediction(model, preprocessed_image)
+    prediction = make_prediction(model, img_batch)
     print(f"Predicted label: {prediction}")
